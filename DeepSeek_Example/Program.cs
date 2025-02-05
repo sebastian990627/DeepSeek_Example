8385c7049bc28a33171bddf2cec3f3c7e70b05cf
Sebastian Bułdak <sebastian990627@gmail.com>
2025-02-05 20:38:34 +0100

    Dodaj pliki projektów.

---
+++ DeepSeek_Example/Program.cs
@@ -0,0 +1,34 @@
+using DeepSeek_Example.Components;
+using Microsoft.Extensions.AI;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddChatClient(new OllamaChatClient
+    (new Uri("http://localhost:11434"),
+    "deepseek-r1:8b"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+var chatClient = app.Services
+    .GetRequiredService<IChatClient>();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
